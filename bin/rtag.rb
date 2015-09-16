a4969fe26b2a86ab5ef79b138888d4f508f8af88
edhowland <ed.howland@gmail.com>
2015-09-16 23:40:39 +0000

    \'mostly working rtag.rb\'

--- bin/rtag.rb
+++ bin/rtag.rb
@@ -1,5 +1,17 @@
-#
+#!/usr/bin/env ruby
+# rtag.rb - ctags like print methods, classes, source file, line number
+
 global_constants =[:Object, :Module, :Class, :BasicObject, :Kernel, :NilClass, :NIL, :Data, :TrueClass, :TRUE, :FalseClass, :FALSE, :Encoding, :Comparable, :Enumerable, :String, :Symbol, :Exception, :SystemExit, :SignalException, :Interrupt, :StandardError, :TypeError, :ArgumentError, :IndexError, :KeyError, :RangeError, :ScriptError, :SyntaxError, :LoadError, :NotImplementedError, :NameError, :NoMethodError, :RuntimeError, :SecurityError, :NoMemoryError, :EncodingError, :SystemCallError, :Errno, :UncaughtThrowError, :ZeroDivisionError, :FloatDomainError, :Numeric, :Integer, :Fixnum, :Float, :Bignum, :Array, :Hash, :ENV, :Struct, :RegexpError, :Regexp, :MatchData, :Marshal, :Range, :IOError, :EOFError, :IO, :STDIN, :STDOUT, :STDERR, :ARGF, :FileTest, :File, :Dir, :Time, :Random, :Signal, :Proc, :LocalJumpError, :SystemStackError, :Method, :UnboundMethod, :Binding, :Math, :GC, :ObjectSpace, :Enumerator, :StopIteration, :RubyVM, :Thread, :TOPLEVEL_BINDING, :ThreadGroup, :Mutex, :ThreadError, :Process, :Fiber, :FiberError, :Rational, :Complex, :RUBY_VERSION, :RUBY_RELEASE_DATE, :RUBY_PLATFORM, :RUBY_PATCHLEVEL, :RUBY_REVISION, :RUBY_DESCRIPTION, :RUBY_COPYRIGHT, :RUBY_ENGINE, :TracePoint, :ARGV, :Gem, :RbConfig, :CROSS_COMPILING, :ConditionVariable, :Queue, :SizedQueue, :MonitorMixin, :Monitor, :RUBYGEMS_ACTIVATION_MONITOR]
 
-require ARGV.first
-p self.class.constants - global_constants
+rfile = ARGV.first
+require rfile
+local_constants =  self.class.constants - global_constants
+
+local_constants.each do |c|
+  con = self.class.const_get c
+  methods = con.instance_methods
+  ancestor = con.ancestors[1] # strange con is its own ancestor
+  this_methods = methods - ancestor.instance_methods
+  #puts "#{con.name} has #{this_methods.length} methods"
+  this_methods.each {|m| puts "#{m}|#{con}|#{rfile}"}
+end
